2908ad54245be3ca54b8b65e1013f3c2e43c7e53
Mitya Kim <mitya_kim@icloud.com>
2021-12-02 16:19:06 -0800

    initial commit

---
+++ server/controllers/auth.js
@@ -0,0 +1,38 @@
+const bcrypt = require('bcryptjs')
+const users = []
+
+module.exports = {
+  login: (req, res) => {
+    const { username, password } = req.body
+    console.log(req.body)
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].username === username) {
+        const authenticated = bcrypt.compareSync(password, users[i].passwordHash)
+        if (authenticated) {
+          let userToReturn = {...users[i]}
+          delete userToReturn.passwordHash
+          res.status(200).send(userToReturn)
+          return
+        }
+      }
+    }
+    res.status(400).send("User not found.")
+  },
+    register: (req, res) => {
+        const { username, email, firstName, lastName, password } = req.body
+        const salt = bcrypt.genSaltSync(5)
+        const passwordHash = bcrypt.hashSync(password, salt)
+        let user = {
+          username,
+          email,
+          firstName,
+          lastName,
+          passwordHash
+        }
+        users.push(user)
+        let userToReturn = {...user}
+        delete userToReturn.passwordHash
+        console.log(users)
+        res.status(200).send(userToReturn)
+    }
+}
